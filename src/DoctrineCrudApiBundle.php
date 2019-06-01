2a494e8fd5f9912aabc19ce5ea5ac2c0e93094ac
Werner Dweight <wd@wernerdweight.com>
2019-06-01 10:39:27 +0200

    🎎 Initial commit

---
+++ src/DoctrineCrudApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace WernerDweight\DoctrineCrudApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DoctrineCrudApiBundle extends Bundle
+{
+}
